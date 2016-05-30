5fed786543ed5b6d3fe368d847096391eb78c723
BtbN <btbn@btbn.de>
2016-05-30 15:56:43 +0200

    Update hardcoded unit

--- weatherweb/static/temp_and_rain.js
+++ weatherweb/static/temp_and_rain.js
@@ -31,7 +31,7 @@ function create_tnr_chart(div_id) {
     chart.showLoading("Loading Data...");
     chart.yAxis[0].remove();
     chart.addAxis({title: {text: "Temperatur - °C"}, id: "temp_axis"});
-    chart.addAxis({title: {text: "Niederschlag - mm/h", style: {"color": "#0018FF"}}, id: "rain_axis", gridLineWidth: 0, opposite: true, min: 0, max: 10.0});
+    chart.addAxis({title: {text: "Niederschlag - mm/10min", style: {"color": "#0018FF"}}, id: "rain_axis", gridLineWidth: 0, opposite: true, min: 0, max: 10.0});
     return chart;
 }
 
