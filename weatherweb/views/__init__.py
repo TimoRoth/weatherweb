4e22732182e13c276de0c829d23b28030b521d6b
BtbN <btbn@btbn.de>
2016-09-20 12:17:16 +0200

    Add external feed api

--- weatherweb/views/__init__.py
+++ weatherweb/views/__init__.py
@@ -2,3 +2,4 @@ from .select_station import *
 from .json import *
 from .admin import *
 from .charts import *
+from .ext import *
---
+++ weatherweb/views/ext.py
@@ -0,0 +1,47 @@
+from flask import request
+from datetime import timedelta
+
+from .. import app
+from ..database import *
+from ..utils.measurements import import_measurement
+from ..utils.pydl15.parse import parse_dl15
+
+
+@app.route("/ext/last_data/<int:station_id>")
+def last_data(station_id):
+    mes = Measurement.query.filter(Measurement.station_id == station_id).order_by(Measurement.datetime.desc()).first()
+    if mes is None:
+        return "Station not found", 404
+    return (mes.datetime + timedelta(minutes=1)).strftime("%Y-%m-%d %H:%M:%S")
+
+
+@app.route("/ext/feed/<int:station_id>", methods=["POST", "GET"])
+@app.route("/ext/feed/<int:station_id>/<auth_string>", methods=["POST", "GET"])
+def feed_data(station_id, auth_string=None):
+    station = Station.query.filter(Station.id == station_id).first()
+    if station is None:
+        return "Station not found", 404
+    if not station.address.startswith("ext"):
+        return "Not an external station", 403
+
+    if station.address.startswith("extauth:"):
+        auth = station.address[8:]
+        if auth != auth_string:
+            return "Invalid auth", 403
+    else:
+        ext = station.address[4:]
+        if ext != request.remote_addr:
+            return "Invalid auth", 403
+
+    if request.method == "GET":
+        data = request.args.get("data")
+    else:
+        data = request.form["data"]
+
+    if data is None:
+        return "No data"
+
+    name, data = parse_dl15(data)
+    cnt = import_measurement(station, data)
+
+    return "Imported %d lines of data" % cnt
