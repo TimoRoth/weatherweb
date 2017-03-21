57643ebaad7d1bf625df7ecbdb79dc19cb3efcfc
BtbN <btbn@btbn.de>
2017-03-21 11:12:12 +0100

    Fix fixup script

--- fix_repo.sh
+++ fix_repo.sh
@@ -3,5 +3,7 @@ cd "$(dirname "$0")"
 shopt -s globstar
 dos2unix **/*.{py,html,js}
 chmod 644 **/*.{py,html,js}
+chmod +x external_feeder/*.py
+chmod -x external_feeder/pydl15.py
 chmod +x *.py
 chmod -x setup.py
