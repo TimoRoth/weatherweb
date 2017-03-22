30304563484723675699e3281217501cec87f3be
BtbN <btbn@btbn.de>
2017-03-22 15:34:12 +0100

    Sort charts

--- weatherweb/static/summary.js
+++ weatherweb/static/summary.js
@@ -31,36 +31,36 @@ function create_summary_chart(div_id, data_url, dura_unit, wind_speed_id, wind_d
 
     var temp_color = "#d95f02";
     var temp_name = "Temperatur - °C";
-    var rain_color = "#0018ff";
-    var rain_name = "Niederschlag - mm/" + dura_unit;
     var wind_color = "#1b9e77";
     var wind_name = "Windgeschwindigkeit - m/s";
     var humi_color = "#7570b3";
     var humi_name = "Luftfeuchtigkeit - %";
     var bila_color = "#e7298a";
     var bila_name = "Strahlungsbilanz - W/m²";
+    var rain_color = "#0018ff";
+    var rain_name = "Niederschlag - mm/" + dura_unit;
 
     chart.showLoading("Lade Daten...");
     chart.yAxis[0].remove();
     chart.addAxis({title: {text: temp_name, style: {"color": temp_color}}, id: "temp_axis"}, false);
-    chart.addAxis({title: {text: rain_name, style: {"color": rain_color}}, id: "rain_axis", gridLineWidth: 0, opposite: true, min: 0, max: 7.5}, false);
     chart.addAxis({title: {text: wind_name, style: {"color": wind_color}}, id: "wind_speed_axis", gridLineWidth: 0, min: 0, opposite: true}, false);
     chart.addAxis({title: {text: humi_name, style: {"color": humi_color}}, id: "humid_axis", gridLineWidth: 0, opposite: true, min: 0, max: 100}, false);
     chart.addAxis({title: {text: bila_name, style: {"color": bila_color}}, id: "bila_axis", gridLineWidth: 0}, false);
+    chart.addAxis({title: {text: rain_name, style: {"color": rain_color}}, id: "rain_axis", gridLineWidth: 0, opposite: true, min: 0, max: 7.5}, false);
     var temp_series = chart.addSeries({name: temp_name, yAxis: "temp_axis", color: temp_color}, false, false);
-    var rain_series = chart.addSeries({name: rain_name, yAxis: "rain_axis", color: rain_color, type: "area"}, false, false);
     var wind_series = chart.addSeries({name: wind_name, yAxis: "wind_speed_axis", color: wind_color}, false, false);
     var humi_series = chart.addSeries({name: humi_name, yAxis: "humid_axis", color: humi_color}, false, false);
     var bila_series = chart.addSeries({name: bila_name, yAxis: "bila_axis", color: bila_color}, false, false);
+    var rain_series = chart.addSeries({name: rain_name, yAxis: "rain_axis", color: rain_color, type: "area"}, false, false);
 
     var update_function = function() {
         chart.redraw(true);
         $.getJSON(data_url, function(data) {
             temp_series.setData(data[temp_id].data, false);
-            rain_series.setData(data[rain_id].data, false);
             wind_series.setData(data[wind_speed_id].data, false);
             humi_series.setData(data[humid_id].data, false);
             bila_series.setData(data[bila_id].data, false);
+            rain_series.setData(data[rain_id].data, false);
             chart.hideLoading();
             chart.redraw(true);
         });
