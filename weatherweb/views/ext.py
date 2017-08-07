57be9f29bf10f0b859491a67257339263dcb0efc
BtbN <btbn@btbn.de>
2017-08-07 15:48:25 +0200

    Test

--- weatherweb/views/ext.py
+++ weatherweb/views/ext.py
@@ -49,5 +49,6 @@ def feed_data(station_id, auth_string=None):
 @app.route("/ext/feed_cr/<int:station_id>/<auth_string>/<file_name>", methods=["PUT", "POST"])
 def feed_cr(station_id, auth_string, file_name):
     with open("/tmp/put_data.txt", "wb") as f:
+        f.write(b"Hallo das ist ein TEST!!!\n")
         f.write(str(request.json).encode('utf-8'))
     return "OK"
