2e517aff3112db51dd62b6a8a78f618e5f7179d2
Timo Rothenpieler <timo.rothenpieler@uni-bremen.de>
2022-08-30 15:11:36 +0200

    Add some highcharts modules

--- weatherweb/assets.py
+++ weatherweb/assets.py
@@ -6,7 +6,12 @@ from . import app
 assets = Environment(app)
 
 default_js = Bundle("jquery.js", filters="jsmin", output="gen/charts.js")
-charts_js = Bundle("jquery.js", "highcharts.js", filters="jsmin", output="gen/packed_charts.js")
+charts_js = Bundle("jquery.js",
+                   "highcharts.js",
+                   "highcharts-exporting.js",
+                   "highcharts-export-data.js",
+                   "highcharts-accessibility.js",
+                   filters="jsmin", output="gen/packed_charts.js")
 css_all = Bundle("style.css", filters="cssmin", output="gen/packed.css")
 
 assets.register("default_js", default_js)
